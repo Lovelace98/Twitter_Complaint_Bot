93588e8cb8a966c131f06d8c5c470c1815c282f9
GHOST <lovelacegmf@gmail.com>
2023-09-24 23:01:32 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+options = Options()
+service = Service()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager(driver_version="116.0.5845.141").install(), options=options))
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+TWITTER_EMAIL = "Client's Twitter Email"
+TWITTER_PASSWORD = "Client's Twitter Password"
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        self.up = 0
+        self.down = 0
+        self.options = Options()
+        self.service = Service()
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install(), options=options))
+        
+    #This grabs the speedtest homepage and clicks on the go button
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+        # accept_button = self.driver.find_element_by_id("_evidon-banner-acceptbutton")
+        # accept_button.click()
+        # time.sleep(3)
+
+        go_button = self.driver.find_element(By.CSS_SELECTOR ,".start-button a")
+        go_button.click()
+        time.sleep(60)
+        self.up = self.driver.find_element(By.XPATH,'//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        self.down = self.driver.find_element(By.XPATH,'//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+
+    #This block of code logs in to twitter, make a tweet. 
+    def tweet_at_provider(self):
+        self.driver.get("https://twitter.com/login")
+        time.sleep(2)
+        email = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
+        password = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
+        email.send_keys(TWITTER_EMAIL)
+        password.send_keys(TWITTER_PASSWORD)
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
+        time.sleep(5)
+        tweet_compose = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+        tweet_button = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button.click()
+        time.sleep(2)
+        self.driver.quit()
+
+
+bot = InternetSpeedTwitterBot()
+bot.get_internet_speed()
+bot.tweet_at_provider()
---
+++ readme.md
@@ -0,0 +1,9 @@
+**This project uses selenium**
+
+## PROBLEM
+A client had a subscription to this internet service provider. They promised him a certain number of download and upload speeds. Sometimes, their internet speeds would fall short of the promised speeds. Anytime this happens, the client has to go to twitter and complain on twitter while tagging them in a tweet. Ofcourse to do this almost everytime you have a slow internet is a boring and tediuos task. Client then sought for help. 
+
+## SOLUTION
+The client reached out to me and i built a bot that checks for his internet speed at the moment and automatically make a tweet tagging his internet service provider. So from now, anytime he experiences a low internet speed, he runs the python script and it automatically makes the complaint on twitter. This twitter bot solves the client needs perfectly. 
+
+See the block by block code explanation in the main.py file. 
